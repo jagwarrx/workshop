4044be1baf6d31ac40f27d22c70fb4a189157dd4
harim <harim>
2024-02-09 09:43:27 -0700

    next

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,104 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<table border="1">
+    <tr>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Company Name</th>
+      <th>Role in Company</th>
+      <th>Address</th>
+      <th>Email</th>
+      <th>Phone Number</th>
+    </tr>
+    <tr>
+      <td>John</td>
+      <td>Smith</td>
+      <td>IT Solutions</td>
+      <td>Analyst</td>
+      <td>98 North Road</td>
+      <td>jsmith@itsolutions.co.uk</td>
+      <td>40716543298</td>
+    </tr>
+    <tr>
+      <td>Jane</td>
+      <td>Dorsey</td>
+      <td>MediCare</td>
+      <td>Medical Engineer</td>
+      <td>11 Crown Street</td>
+      <td>jdorsey@mc.com</td>
+      <td>40791345621</td>
+    </tr>
+    <tr>
+      <td>Albert</td>
+      <td>Kipling</td>
+      <td>Waterfront</td>
+      <td>Accountant</td>
+      <td>22 Guild Street</td>
+      <td>kipling@waterfront.com</td>
+      <td>40735416854</td>
+    </tr>
+    <tr>
+      <td>Michael</td>
+      <td>Robertson</td>
+      <td>MediCare</td>
+      <td>IT Specialist</td>
+      <td>17 Farburn Terrace</td>
+      <td>mrobertson@mc.com</td>
+      <td>40733652145</td>
+    </tr>
+    <tr>
+      <td>Doug</td>
+      <td>Derrick</td>
+      <td>Timepath Inc.</td>
+      <td>Analyst</td>
+      <td>99 Shire Oak Road</td>
+      <td>dderrick@timepath.co.uk</td>
+      <td>40799885412</td>
+    </tr>
+    <tr>
+      <td>Jessie</td>
+      <td>Marlowe</td>
+      <td>Aperture Inc.</td>
+      <td>Scientist</td>
+      <td>27 Cheshire Street</td>
+      <td>jmarlowe@aperture.us</td>
+      <td>40733154268</td>
+    </tr>
+    <tr>
+      <td>Stan</td>
+      <td>Hamm</td>
+      <td>Sugarwell</td>
+      <td>Advisor</td>
+      <td>10 Dam Road</td>
+      <td>shamm@sugarwell.org</td>
+      <td>40712462257</td>
+    </tr>
+    <tr>
+      <td>Michelle</td>
+      <td>Norton</td>
+      <td>Aperture Inc.</td>
+      <td>Scientist</td>
+      <td>13 White Rabbit Street</td>
+      <td>mnorton@aperture.us</td>
+      <td>40731254562</td>
+    </tr>
+    <tr>
+      <td>Stacy</td>
+      <td>Shelby</td>
+      <td>TechDev</td>
+      <td>HR Manager</td>
+      <td>19 Pineapple Boulevard</td>
+      <td>sshelby@techdev.com</td>
+      <td>40741785214</td>
+    </tr>
+    <tr>
+      <td>Lara</td>
+      <td>Palmer</td>
+      <td>Timepath Inc.</td>
+      <td>Programmer</td>
+      <td>87 Orange Street</td>
+      <td>lpalmer@timepath.co.uk</td>
+      <td>40731653845</td>
+    </tr>
+  </table>
+  
